e0ee8236a1351001ca3c6cd038f90987a7f3474b
unknown <kumar.aqua@gmail.com>
2013-10-21 14:12:32 +0100

    Simple chnage

--- src/main/java/com/yummynoodlebar/config/CoreConfig.java
+++ src/main/java/com/yummynoodlebar/config/CoreConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
+
 import java.util.UUID;
 
 @Configuration
